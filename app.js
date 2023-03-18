576e937856aee27abd6ab916ecc4ccb60e65a44a
Michael Lei <michaelleie0ca@gmail.com>
2023-03-18 02:45:24 -0700

    Compress all routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const logger = require("morgan");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
+const compression = require("compression");
 
 const app = express();
 
@@ -22,6 +23,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
